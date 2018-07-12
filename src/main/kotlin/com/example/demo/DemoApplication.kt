1de69928ee724d2600b3e0dad45bb9c593d9fcd9
exosyphon <exosyphon@yahoo.com>
2018-07-11 23:16:10 -0500

    Use jdbc batch to optimize writes
    finished : 11985 MS

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,9 +1,13 @@
 package com.example.demo
 
+import org.springframework.batch.item.ItemWriter
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider
+import org.springframework.batch.item.database.JdbcBatchItemWriter
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import javax.sql.DataSource
 
 @SpringBootApplication
 class DemoApplication {
@@ -14,11 +18,19 @@ class DemoApplication {
         thingService.truncate()
         println("beginning")
         val time = System.currentTimeMillis()
-        thingService.doThings()
+        thingService.doThingsFaster()
         val time2 = System.currentTimeMillis()
         println("finished : ${time2 - time} MS")
     }
 
+    @Bean
+    fun dbWriter(dataSource: DataSource): ItemWriter<Thing> {
+        val writer = JdbcBatchItemWriter<Thing>()
+        writer.setItemSqlParameterSourceProvider(BeanPropertyItemSqlParameterSourceProvider<Thing>())
+        writer.setSql("INSERT INTO thing (name, second_name) VALUES (:name,:secondName)")
+        writer.setDataSource(dataSource)
+        return writer
+    }
 }
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/example/demo/ThingService.kt
+++ src/main/kotlin/com/example/demo/ThingService.kt
@@ -1,12 +1,21 @@
 package com.example.demo
 
+import org.springframework.batch.item.ItemWriter
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import javax.persistence.EntityManager
 
 @Service
-class ThingService(val entityManager: EntityManager, val thingRepository: ThingRepository) {
+class ThingService(val entityManager: EntityManager, val thingRepository: ThingRepository, val itemWriter: ItemWriter<Thing>) {
+
+    fun doThingsFaster() {
+        var list = mutableListOf<Thing>()
+        for (i in 0..500_000) {
+            list.add(Thing("$i", "name $i"))
+        }
+        itemWriter.write(list)
+    }
 
     @Transactional
     @Modifying
